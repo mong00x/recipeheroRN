1cc4a8be554bc63a844e328edf9c736c8b07017a
Meng Yuan <13714858@student.uts.edu.au>
2021-11-01 01:00:56 +0930

    improve home screen ui with native base

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,16 @@
 import React, { useState, useEffect } from "react";
 
 import { StyleSheet, TouchableOpacity } from "react-native";
-import { Center, VStack, HStack, Heading, Text, View } from "native-base";
+import {
+  Box,
+  Circle,
+  Center,
+  VStack,
+  Flex,
+  Heading,
+  Text,
+  View,
+} from "native-base";
 
 import { Ionicons } from "@expo/vector-icons";
 import { auth } from "../firebase";
@@ -10,28 +19,24 @@ import { database } from "../firebase";
 import { useNavigation } from "@react-navigation/core";
 import { SafeAreaView } from "react-native-safe-area-context";
 
+import ShoppingList from "../components/ShoppingList";
+
 const HomeScreen = () => {
-  const [name, setName] = useState();
-  const dbRef = database.ref("hello/");
-  console.log(dbRef);
+  const [shopList, setShopList] = useState(null);
 
   const navigation = useNavigation();
   const user = auth.currentUser;
 
   useEffect(() => {
-    const getData = async () => {
-      await dbRef.once("value").then((snap) => {
-        const data = snap.val();
-        updateData(data);
-      });
-    };
-    getData();
-  }, []);
+    const dbRef = database.ref("hello/");
 
-  const updateData = (data) => {
-    const D = data;
-    console.log(D);
-  };
+    dbRef.on("value", (snapshot) => {
+      setShopList(snapshot.val());
+    });
+    return () => {
+      dbRef.off();
+    };
+  });
 
   const handleSignOut = () => {
     auth.signOut().then(() => {
@@ -51,53 +56,23 @@ const HomeScreen = () => {
   };
 
   return (
-    <Center flex={1} px="3">
-      <SafeAreaView>
-        <VStack flex="1" justifyContent="space-around">
-          <HStack>
-            <Heading>Hi,{auth.currentUser?.email}</Heading>
-            <TouchableOpacity style={[styles.button]} onPress={handleSignOut}>
-              <Text style={styles.buttonText}>Sign out</Text>
-              <Ionicons name="log-out-outline" size={36} color="white" />
-            </TouchableOpacity>
-          </HStack>
-
-          {/* {shopList ? (
-            <View>
-              <Text>You have one on-going shopping list</Text>
-              <Center>
-                <TouchableOpacity onPress={addData}>
-                  <Ionicons name="add-circle-outline" size={36} color="#333" />
-                </TouchableOpacity>
-              </Center>
-            </View>
-          ) : (
-            <TouchableOpacity style={styles.button} onPress={addList}>
-              <Text style={styles.buttonText}>Add new Shopping List</Text>
-            </TouchableOpacity>
-          )} */}
-        </VStack>
-      </SafeAreaView>
-    </Center>
+    <SafeAreaView>
+      <Box mt={3} mx={5}>
+        <Flex direction="row" justifyContent="space-between" flexWrap="wrap">
+          <Heading>Hi, {auth.currentUser?.email}</Heading>
+          <TouchableOpacity onPress={handleSignOut}>
+            <Circle size={36} bg="secondary.400">
+              <Ionicons name="log-out-outline" size={24} color="white" />
+            </Circle>
+          </TouchableOpacity>
+        </Flex>
+        <ShoppingList />
+        {/* <TouchableOpacity onPress={addList}>
+          <Text>Create a new Shopping List</Text>
+        </TouchableOpacity> */}
+      </Box>
+    </SafeAreaView>
   );
 };
 
 export default HomeScreen;
-
-const styles = StyleSheet.create({
-  button: {
-    flex: 1,
-    flexDirection: "row",
-    backgroundColor: "#333",
-    padding: 5,
-    borderRadius: 50,
-    justifyContent: "center",
-    alignItems: "center",
-    maxHeight: 40,
-  },
-  buttonText: {
-    color: "white",
-    fontSize: 16,
-    fontWeight: "700",
-  },
-});
